91c5b40ead6ae0425541bc0580a3d2762c1bce8f
Hamidputrawan33 <terhuracloth@gmail.com>
2023-04-03 13:09:38 +0700

    first commit

---
+++ lib/model/database.dart
@@ -0,0 +1,39 @@
+List<Map> pegawai = [
+  {
+    'id': 1,
+    'nip': '545664',
+    'nama': 'Putrawan',
+    'tanggalLahir': '15 Oktober 2002',
+    'nomorTelepon': '085083052861',
+    'email': 'putrawan@gmail.com',
+    'password': '111',
+  },
+  {
+    'id': 2,
+    'nip': '6576455',
+    'nama': 'Rizky ',
+    'tanggalLahir': '9 Juli 2004',
+    'nomorTelepon': '085665765490',
+    'email': 'rizky@gmail.com',
+    'password': '123',
+  },
+];
+
+List<Map> pasien = [
+  {
+    'id': 1,
+    'noRm': '11877',
+    'nama': 'Budi Santoso',
+    'tanggalLahir': '12 Oktober 2000',
+    'nomorTelepon': '08188767459',
+    'alamat': 'Blora',
+  },
+  {
+    'id': 2,
+    'noRm': '7679',
+    'nama': 'Surya',
+    'tanggalLahir': '20 Juni 2001',
+    'nomorTelepon': '081986563761',
+    'alamat': 'Pati',
+  },
+];
